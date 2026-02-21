2190e1e9b6bf6d60168cb130557e6ed6b98c5727
pk6610 <paulkarlik@gmail.com>
2026-02-20 23:37:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, jsonify
 import asyncio
-import holehe.modules as modules
 from holehe.core import *
+from httpx import AsyncClient
 
 app = Flask(__name__)
 
 @app.route('/check/<email>')
-async def check_email(email):
-    results = []
-    client = AsyncClient()
-    websites = get_functions()
-    for website in websites:
-        result = await website(email, client, results)
-    await client.aclose()
+def check_email(email):
+    async def run():
+        data = []
+        client = AsyncClient()
+        websites = get_functions()
+        for website in websites:
+            try:
+                await website(email, client, data)
+            except Exception:
+                pass
+        await client.aclose()
+        return data
+    results = asyncio.run(run())
     return jsonify(results)
 
 if __name__ == '__main__':
